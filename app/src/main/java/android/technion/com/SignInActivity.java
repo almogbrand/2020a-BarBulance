af52731e94fa6b320c3df0678b4e55738f0fbdfe
Lilit Yenokyan <lilit.yenokyan@solid-run.com>
2019-12-09 20:23:19 +0200

    Add sign in activity
    Signed-off-by: Lilit Yenokyan <lilit.yenokyan@solid-run.com>

---
+++ app/src/main/java/android/technion/com/SignInActivity.java
@@ -0,0 +1,36 @@
+package android.technion.com;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class SignInActivity extends AppCompatActivity {
+    private EditText signInUser;
+    private EditText signInPassword;
+    private Button signInButton;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_in);
+
+        signInUser = findViewById(R.id.signInUser);
+        signInPassword = findViewById(R.id.signInPassword);
+        signInButton = findViewById(R.id.signInButton);
+
+        signInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+//                Intent intent = new Intent(SignInActivity.this, EventsActivity.class);
+                finish();
+//                startActivity(intent);
+            }
+        });
+
+    }
+}
