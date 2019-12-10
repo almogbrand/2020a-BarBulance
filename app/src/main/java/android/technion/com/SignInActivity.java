4eb6e65171ecdfdee2a9138016df0818a60c47c2
Almog <almogb001@gmail.com>
2019-12-10 11:26:05 +0200

    initial commit
    Signed-off-by: Almog <almogb001@gmail.com>

--- app/src/main/java/android/technion/com/SignInActivity.java
+++ app/src/main/java/android/technion/com/SignInActivity.java
@@ -1,14 +1,15 @@
 package android.technion.com;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
 
 public class SignInActivity extends AppCompatActivity {
     private EditText signInUser;
@@ -32,7 +33,7 @@ public class SignInActivity extends AppCompatActivity {
             public void onClick(View v) {
                 // TO DO: add permissions here
                 Intent intent = new Intent(SignInActivity.this, EventsActivity.class);
-                finish();
+//                finish();
                 startActivity(intent);
             }
         });
