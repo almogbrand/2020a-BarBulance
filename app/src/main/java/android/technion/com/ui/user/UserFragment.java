f31ae56e70b8e9335acc480cf88c9bfca147d655
Almog <almogb001@gmail.com>
2019-12-23 17:33:16 +0200

    User Profile activity - Showing full name from FACEBOOK account. ToDo - 1. replace local String field with database field 2. add an option to enter phone number and save to database 3. get profile pictrue from Facebook
    Signed-off-by: Almog <almogb001@gmail.com>

--- app/src/main/java/android/technion/com/ui/user/UserFragment.java
+++ app/src/main/java/android/technion/com/ui/user/UserFragment.java
@@ -5,6 +5,9 @@ import android.technion.com.R;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.textservice.TextInfo;
+import android.widget.AutoCompleteTextView;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import androidx.annotation.Nullable;
@@ -13,22 +16,49 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class UserFragment extends Fragment {
 
     private UserViewModel notificationsViewModel;
+    private FirebaseAuth mAuth;
+
+    //TEMP - TO BE DELETED AFTER CONNECTING TO db
+    String name = "";
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
+
+        mAuth = FirebaseAuth.getInstance();
+
         notificationsViewModel =
                 ViewModelProviders.of(this).get(UserViewModel.class);
         View root = inflater.inflate(R.layout.fragment_user, container, false);
-        final TextView textView = root.findViewById(R.id.text_notifications);
-        notificationsViewModel.getText().observe(this, new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
+//        final TextView textView = root.findViewById(R.id.text_notifications);
+//        notificationsViewModel.getText().observe(this, new Observer<String>() {
+//            @Override
+//            public void onChanged(@Nullable String s) {
+//                textView.setText(s);
+//            }
+//        });
+
+        //Update name field from Facebook's data
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        name = currentUser.getDisplayName();    // CHANGE TO db FIELD
+        TextView view = root.findViewById(R.id.userName);
+        view.setText(name);
+
         return root;
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        name = currentUser.getDisplayName();
     }
 }
