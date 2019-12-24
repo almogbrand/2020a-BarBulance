7f41e000b8f89aad8ce632a8a36f818d6cbc678a
Almog <almogb001@gmail.com>
2019-12-24 19:20:33 +0200

    #15 update user profile fragment
    Signed-off-by: Almog <almogb001@gmail.com>

--- app/src/main/java/android/technion/com/ui/user/UserFragment.java
+++ app/src/main/java/android/technion/com/ui/user/UserFragment.java
@@ -1,6 +1,10 @@
 package android.technion.com.ui.user;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.technion.com.R;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -8,7 +12,9 @@ import android.view.ViewGroup;
 import android.view.textservice.TextInfo;
 import android.widget.AutoCompleteTextView;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.annotation.NonNull;
@@ -16,10 +22,17 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
+import com.facebook.AccessToken;
+import com.facebook.login.widget.ProfilePictureView;
 import com.google.android.material.textfield.TextInputEditText;
 import com.google.android.material.textfield.TextInputLayout;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.squareup.picasso.Picasso;
+
+import java.net.URL;
+
+
 
 public class UserFragment extends Fragment {
 
@@ -27,7 +40,8 @@ public class UserFragment extends Fragment {
     private FirebaseAuth mAuth;
 
     //TEMP - TO BE DELETED AFTER CONNECTING TO db
-    String name = "";
+     String user_name = "";
+     String user_email = "";
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -45,11 +59,30 @@ public class UserFragment extends Fragment {
 //            }
 //        });
 
-        //Update name field from Facebook's data
+
         FirebaseUser currentUser = mAuth.getCurrentUser();
-        name = currentUser.getDisplayName();    // CHANGE TO db FIELD
-        TextView view = root.findViewById(R.id.userName);
-        view.setText(name);
+
+        if(currentUser != null) {
+            {
+                //Update name field from Facebook's data
+                user_name = currentUser.getDisplayName();    // CHANGE TO db FIELD
+                TextView name_view = root.findViewById(R.id.userName);
+                name_view.setText(user_name);
+                name_view.setFocusable(false);
+
+                //Update Email field from Facebook's data
+                user_email = currentUser.getEmail();    // CHANGE TO db FIELD
+                TextView email_view = root.findViewById(R.id.userEmail);
+                email_view.setText(user_email);
+                email_view.setFocusable(false);
+
+                //Update user profile picture
+                ImageView imgView = root.findViewById(R.id.profile_image);
+                Uri imgUri = currentUser.getPhotoUrl();
+                imgView.setImageURI(imgUri);
+                Picasso.with(getActivity()).load(imgUri).into(imgView);
+            }
+        }
 
         return root;
     }
@@ -59,6 +92,15 @@ public class UserFragment extends Fragment {
         super.onStart();
         // Check if user is signed in (non-null) and update UI accordingly.
         FirebaseUser currentUser = mAuth.getCurrentUser();
-        name = currentUser.getDisplayName();
+
+        if(currentUser != null){
+            user_name = currentUser.getDisplayName();
+        }
+//        if(!currentUser.getDisplayName().equals(null)){
+//            user_name = currentUser.getDisplayName();
+//        }
+
+
+
     }
 }
