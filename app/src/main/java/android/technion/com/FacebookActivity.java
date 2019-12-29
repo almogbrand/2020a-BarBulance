212431b9d84497b62f9f58b47999dc33ab1b65e3
Almog <almogb001@gmail.com>
2019-12-29 22:25:38 +0200

    removing singin button

--- app/src/main/java/android/technion/com/FacebookActivity.java
+++ app/src/main/java/android/technion/com/FacebookActivity.java
@@ -124,15 +124,15 @@ public class FacebookActivity extends AppCompatActivity {
         });
 
         /* Temporary bypassing facebook login *////////////////////////////////////////
-        signInButton = findViewById(R.id.signInButton);
-        signInButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(FacebookActivity.this, MainActivity.class);
-                finish();
-                startActivity(intent);
-            }
-        });
+//        signInButton = findViewById(R.id.signInButton);
+//        signInButton.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Intent intent = new Intent(FacebookActivity.this, MainActivity.class);
+//                finish();
+//                startActivity(intent);
+//            }
+//        });
         //////////////////////////////////////////////////////////////////////////////
 
     }
